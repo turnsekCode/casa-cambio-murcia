9e758bda16ce45aa6a4f1f5313fe26e63b158f89
abraham <abraham.turnsek@gmail.com>
2023-10-24 14:21:39 +0200

    quitar espacio en numero de telefono

--- .next/static/82_iskSNDCqppNe5WfC8W/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/25-0606bbedcc4ae4df.js","static/css/8501ca4e02a601db.css","static/chunks/pages/index-3e40c5ddc70fd706.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ .next/static/Sz5o274XDLF47pzBDZuRW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/25-0606bbedcc4ae4df.js","static/css/8501ca4e02a601db.css","static/chunks/pages/index-972022344f2ef9c0.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
